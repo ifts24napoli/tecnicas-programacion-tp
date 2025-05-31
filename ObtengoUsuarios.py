3c8d6693f91f179d792da7479dbb8bd218b6c664
Yamil Arribas <25697097@ifts24.edu.ar>
2025-05-30 21:39:08 -0300

    Prueba Conexion

---
+++ ObtengoUsuarios.py
@@ -0,0 +1,13 @@
+from BaseDeDatos.Eda import eleiminar, actualizar, insertar, consultar
+ 
+    
+def main():
+     #Ejemolos filtro Texto eleiminar("alumnos", "apellido", "'Lusto'")
+     #Ejemplo filtro entero eleiminar("alumnos","apellido", 54)
+    # eleiminar("alumnos", "alumnoId", 66)
+    # actualizar("alumnos", "alumnoID", "64" , Nombre = "Martin", Apellido = "Lista", Edad = 44, Grado = "Secundario")
+    insertar("alumnos", Apellido = 'Alarcon', Nombre = 'Roxana',  Edad = 38, Grado = "Primero")
+    # consultar("Select * from alumnos where nombre = 'Yamil'")
+       
+if __name__ == "__main__":
+    main()            
