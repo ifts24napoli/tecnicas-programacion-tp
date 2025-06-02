bdbba54fef8264bf0547559511bf642901ba77ce
Yamil Arribas <25697097@ifts24.edu.ar>
2025-06-02 20:51:43 -0300

    Archivo para acceso a DB sin Eda

--- PruebaDB.py
+++ PruebaDB.py
@@ -1,15 +1,15 @@
-# Este es un modulo para ralizar Pruebas de AMB en la base de datos
+from BaseDeDatos.DBManager import DBManager
 
-from BaseDeDatos.Eda import eleiminar, actualizar, insertar, consultar
- 
-    
-def main():
-     #Ejemolos filtro Texto eleiminar("alumnos", "apellido", "'Lusto'")
-     #Ejemplo filtro entero eleiminar("alumnos","apellido", 54)
-    # eleiminar("alumnos", "alumnoId", 66)
-    # actualizar("alumnos", "alumnoID", "64" , Nombre = "Martin", Apellido = "Lista", Edad = 44, Grado = "Secundario")
-    insertar("alumnos", Apellido = 'Alarcon', Nombre = 'Roxana',  Edad = 39, Grado = "Primero")
-    # consultar("Select * from alumnos where nombre = 'Yamil'")
-       
-if __name__ == "__main__":  # Evita que el código se ejecute automáticamente si el archivo es importado como módulo
-    main()            
+db = DBManager()
+consulta = db.consultar('Select * from alumnos')
+if "Error" in consulta:
+    print(consulta)
+else:    
+    for item in consulta:
+        print(item)
+
+actualizacion = db.agregar("insert into alumnos (nombre, apellido)  values ('Yami', 'Arribas')")    
+print(actualizacion)  
+
+  
+
