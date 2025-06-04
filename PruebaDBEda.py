15c315d1b06ce5e0fcabdce6421c987b41b2d5f5
Yamil Arribas <25697097@ifts24.edu.ar>
2025-06-04 12:11:31 -0300

    Cambios para utilizar objetos

--- PruebaDBEda.py
+++ PruebaDBEda.py
@@ -1,15 +1,23 @@
 # Este es un modulo para ralizar Pruebas de AMB en la base de datos
 
 from BaseDeDatos.Eda import eleiminar, actualizar, insertar, consultar
+from Alumnos import Alumnos
  
     
 def main():
+    # Fuera de Servicio
      #Ejemolos filtro Texto eleiminar("alumnos", "apellido", "'Lusto'")
      #Ejemplo filtro entero eleiminar("alumnos","apellido", 54)
-    # eleiminar("alumnos", "alumnoId", 66)
+    # eleiminar("alumnos", "alumnoId", 51)
     # actualizar("alumnos", "alumnoID", "64" , Nombre = "Martin", Apellido = "Lista", Edad = 44, Grado = "Secundario")
-    insertar("alumnos", Apellido = 'Alarcon', Nombre = 'Roxana',  Edad = 39, Grado = "Primero")
+    # insertar("alumnos", Apellido = 'Alarcon', Nombre = 'Roxana',  Edad = 39, Grado = "Primero")
     # consultar("Select * from alumnos where nombre = 'Yamil'")
-       
+    
+    alumnos = Alumnos(Nombre = "otro1", Apellido = "Ponpin1otro", Edad = "19", Grado = "Uni")
+    # insertar("alumnos", **alumnos.__dict__)
+    # actualizar("alumnos", "alumnoid", '77', **alumnos.__dict__)
+    # eleiminar("alumnos", "alumnoid", 79) 
+    # consultar("Select * from alumnos where nombre = 'Yamil'")
+    
 if __name__ == "__main__":  # Evita que el código se ejecute automáticamente si el archivo es importado como módulo
     main()            
