079f063bf060d22b7063de181d9eba4df7f74619
Yamil Arribas <25697097@ifts24.edu.ar>
2025-06-09 11:05:32 -0300

    Creación de Usuarios

--- PruebaDBEda.py
+++ PruebaDBEda.py
@@ -1,7 +1,7 @@
 # Este es un modulo para ralizar Pruebas de AMB en la base de datos
 
 from BaseDeDatos.Eda import eleiminar, actualizar, insertar, consultar
-from Alumnos import Alumnos
+from modelo.Alumnos import Alumnos
  
     
 def main():
@@ -14,12 +14,12 @@ def main():
     nom = input("Ingrese nombr")
     apell = input("Apellido")
     alumnos = Alumnos(Nombre = nom, Apellido = apell)
-    insertar("alumnos", **alumnos.__dict__) #**alumnos.__dict__ convierto el objeto a Diccionario
+    # insertar("alumnos", **alumnos.__dict__) #**alumnos.__dict__ convierto el objeto a Diccionario
     # actualizar("alumnos", "alumnoid", '10', **alumnos.__dict__)
       #Ejemolos filtro Texto eleiminar("alumnos", "apellido", "'Lusto'")
       #Ejemplo filtro entero eleiminar("alumnos","apellido", 54) 
     # eleiminar("alumnos", "alumnoid", 79) 
-    # consultar("Select * from alumnos where nombre = 'Yamil'")
+    consultar("Select * from usuarios")
     
 if __name__ == "__main__":  # Evita que el código se ejecute automáticamente si el archivo es importado como módulo
     main()            
--- vistas/MenuPrincipal.py
+++ vistas/MenuPrincipal.py
@@ -1,8 +1,14 @@
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from controlador.Alumnos import agregar,actualiza
-from modelo.Alumnos import Alumnos
+from vistas.ABMUsuario import abm
 
-alumnos = Alumnos(Nombre = "Pepesssss", Apellido = "otssssro")
-agregar(alumnos)
-# actualiza("alumnoid", 17, alumnos)
+while True:
+    print(""" Selecciones una de las siguientes opciones: \n 
+          0 Para Salir \n
+          1 Gestion de Ususario \n
+          """)
+    opcion = int(input("Ingrese Alguna Opcion: "))
+    if opcion == 0:
+        break
+    elif opcion == 1:
+        abm()
