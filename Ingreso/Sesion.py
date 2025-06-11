d7798e07d5b9651b21150385026a9238c2b06d9a
mnapo <napolimatiase@gmail.com>
2025-06-11 08:06:46 -0300

    Crear metodos conectar y desconectar

--- Ingreso/Sesion.py
+++ Ingreso/Sesion.py
@@ -1,3 +1,13 @@
 class Sesion:
-    conectado = False
-    nombre_usuario = ""
+    def __init__(self):
+        self.conectado = False
+        self.email_usuario = ""
+        self.rol_usuario = 0
+
+    def conectar(self, email, rol):
+        self.conectado = True
+        self.email_usuario = email
+        self.rol_usuario = rol
+
+    def desconectar(self):
+        self.conectado = False
