ba4affb85e2e929ba5a0acc4d7693d507d9f0d6b
mnapo <napolimatiase@gmail.com>
2025-06-17 18:05:43 -0300

    Ingreso y Clientes

--- modelo/Clientes.py
+++ modelo/Clientes.py
@@ -2,6 +2,7 @@ class Clientes:
     def __init__(self,**datos):
         self.nombre = datos.get("Nombre")
         self.apellido = datos.get("Apellido")
+        self.dni = datos.get("DNI")
+        self.cuit = datos.get("Cuit")
         self.mail = datos.get("Mail")
-        self.id_rol = datos.get("Cuit")
-        self.id_rol = datos.get("Dni")
+        self.direccion = datos.get("Direccion")
