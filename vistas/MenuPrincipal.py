b4e62e1e882b6f1e5bf3782bfb2f59ef31c3deda
mnapo <napolimatiase@gmail.com>
2025-06-23 07:07:10 -0300

    Redimensionar altura dinámicamente según rol

--- vistas/MenuPrincipal.py
+++ vistas/MenuPrincipal.py
@@ -11,8 +11,11 @@ from vistas.ABMFacturacion import abmFacturacion
 from vistas.ABMInventario import abmInventario
 from vistas.ABMPlanes import abmPlanes
 from vistas.ABMTipoPago import abmTipoPago
-from vistas.ReporteFinanciero import menu as reporteFinanciero 
+from vistas.ReporteFinanciero import menu as reporteFinanciero
 
+ALTO_FILA = 35
+ALTO_MARCO_USUARIO = 90
+ANCHO_VENTANA = 230
 STICKY_PROP = "w"
 TEXTO_CERRAR_SESION = "Cerrar Sesión"
 TEXTO_CLIENTES = "Clientes"
@@ -73,12 +76,11 @@ def GuiMenuPrincipal(sesion:Sesion):
     from Ingreso import main
     root = Tk()
     root.title(VENTANA_TITULO)
+    root.resizable(0, 0)
     frm = ttk.Frame(root, padding=10)
     frm.grid()
     marco_usuario = ttk.Frame(frm)
     marco_usuario.pack(side="top")
-    root.resizable(width=False, height=False)
-    root.geometry("250x400")
     texto_sesion_usuario = ttk.Label(marco_usuario, text=TEXTO_SESION_USUARIO+sesion.nombre_usuario)
     texto_sesion_usuario.grid(column=0, row=1, sticky=STICKY_PROP)
     texto__ROLES = ttk.Label(marco_usuario, text=TEXTO_ROLES+sesion.tipo_rol)
@@ -89,56 +91,56 @@ def GuiMenuPrincipal(sesion:Sesion):
         main()
     btn_cerrar_sesion = ttk.Button(marco_usuario, text=TEXTO_CERRAR_SESION, command=cerrar_sesion)
     btn_cerrar_sesion.grid(column=0, row=3, sticky=STICKY_PROP)
+
     marco_gestion = ttk.Frame(frm)
     marco_gestion.pack(side="top")
-    btn_usuarios = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_USUARIOS, command=MenuUsuario)
-    btn_usuarios.grid(column=0, row=3, sticky=STICKY_PROP)
-    btn_clientes = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_CLIENTES, command=MenuCliente)
-    btn_clientes.grid(column=0, row=4, sticky=STICKY_PROP)
-    btn_planes = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_PLANES, command=lambda: abmPlanes())
-    btn_planes.grid(column=0, row=5, sticky=STICKY_PROP)
-    btn_inventario = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_INVENTARIO, command=lambda: abmInventario())
-    btn_inventario.grid(column=0, row=6, sticky=STICKY_PROP)
-    btn_tipo_pagos = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_TIPOS_PAGOS, command=lambda: abmTipoPago())
-    btn_tipo_pagos.grid(column=0, row=7, sticky=STICKY_PROP)
-    btn_contratos = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_CONTRATO, command=lambda: abmContratos())
-    btn_contratos.grid(column=0, row=8, sticky=STICKY_PROP)
-    btn_comodatos = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_COMODATOS, command=lambda: abmComodato())
-    btn_comodatos.grid(column=0, row=9, sticky=STICKY_PROP)
-    btn_facturas = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_FACTURAS, command=lambda: abmFacturacion())
-    btn_facturas.grid(column=0, row=10, sticky=STICKY_PROP)
-    btn_reportes = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_REPORTES, command=lambda: reporteFinanciero())
-    btn_reportes.grid(column=0, row=11, sticky=STICKY_PROP)
-    
-    btn_usuarios.config(state=STATE_DISABLED)
-    btn_clientes.config(state=STATE_DISABLED)
-    btn_planes.config(state=STATE_DISABLED)
-    btn_inventario.config(state=STATE_DISABLED)
-    btn_tipo_pagos.config(state=STATE_DISABLED)
-    btn_contratos.config(state=STATE_DISABLED)
-    btn_comodatos.config(state=STATE_DISABLED)
-    btn_facturas.config(state=STATE_DISABLED)
-    btn_reportes.config(state=STATE_DISABLED)
-
+    total_filas = 0
     match sesion.rol_usuario:
-        case 1: 
-            btn_usuarios.config(state=STATE_ENABLED)
-            btn_clientes.config(state=STATE_ENABLED)
-            btn_planes.config(state=STATE_ENABLED)
-            btn_inventario.config(state=STATE_ENABLED)
-            btn_tipo_pagos.config(state=STATE_ENABLED)
-            btn_contratos.config(state=STATE_ENABLED)
-            btn_comodatos.config(state=STATE_ENABLED)
-            btn_facturas.config(state=STATE_ENABLED)
-            btn_reportes.config(state=STATE_ENABLED)
+        case 1:
+            total_filas = 9
+            btn_usuarios = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_USUARIOS, command=MenuUsuario)
+            btn_usuarios.grid(column=0, row=1, sticky=STICKY_PROP)
+            btn_clientes = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_CLIENTES, command=MenuCliente)
+            btn_clientes.grid(column=0, row=2, sticky=STICKY_PROP)
+            btn_planes = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_PLANES, command=lambda: abmPlanes())
+            btn_planes.grid(column=0, row=3, sticky=STICKY_PROP)
+            btn_inventario = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_INVENTARIO, command=lambda: abmInventario())
+            btn_inventario.grid(column=0, row=4, sticky=STICKY_PROP)
+            btn_tipo_pagos = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_TIPOS_PAGOS, command=lambda: abmTipoPago())
+            btn_tipo_pagos.grid(column=0, row=5, sticky=STICKY_PROP)
+            btn_contratos = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_CONTRATO, command=lambda: abmContratos())
+            btn_contratos.grid(column=0, row=6, sticky=STICKY_PROP)
+            btn_comodatos = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_COMODATOS, command=lambda: abmComodato())
+            btn_comodatos.grid(column=0, row=7, sticky=STICKY_PROP)
+            btn_facturas = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_FACTURAS, command=lambda: abmFacturacion())
+            btn_facturas.grid(column=0, row=8, sticky=STICKY_PROP)
+            btn_reportes = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_REPORTES, command=lambda: reporteFinanciero())
+            btn_reportes.grid(column=0, row=9, sticky=STICKY_PROP)
         case 2:
-            btn_clientes.config(state=STATE_ENABLED)
-            btn_planes.config(state=STATE_ENABLED)
-            btn_inventario.config(state=STATE_ENABLED)
-            btn_tipo_pagos.config(state=STATE_ENABLED)
-            btn_contratos.config(state=STATE_ENABLED)
-            btn_comodatos.config(state=STATE_ENABLED)
+            total_filas = 6
+            btn_clientes = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_CLIENTES, command=MenuCliente)
+            btn_clientes.grid(column=0, row=1, sticky=STICKY_PROP)
+            btn_planes = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_PLANES, command=lambda: abmPlanes())
+            btn_planes.grid(column=0, row=2, sticky=STICKY_PROP)
+            btn_inventario = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_INVENTARIO, command=lambda: abmInventario())
+            btn_inventario.grid(column=0, row=3, sticky=STICKY_PROP)
+            btn_tipo_pagos = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_TIPOS_PAGOS, command=lambda: abmTipoPago())
+            btn_tipo_pagos.grid(column=0, row=4, sticky=STICKY_PROP)
+            btn_contratos = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_CONTRATO, command=lambda: abmContratos())
+            btn_contratos.grid(column=0, row=5, sticky=STICKY_PROP)
+            btn_comodatos = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_COMODATOS, command=lambda: abmComodato())
+            btn_comodatos.grid(column=0, row=6, sticky=STICKY_PROP)
         case 3:
-            btn_facturas.config(state=STATE_ENABLED)
-            btn_reportes.config(state=STATE_ENABLED)
+            total_filas = 2
+            btn_facturas = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_FACTURAS, command=lambda: abmFacturacion())
+            btn_facturas.grid(column=0, row=1, sticky=STICKY_PROP)
+            btn_reportes = ttk.Button(marco_gestion, text=TEXTO_GESTION+TEXTO_REPORTES, command=lambda: reporteFinanciero())
+            btn_reportes.grid(column=0, row=2, sticky=STICKY_PROP)
+    ancho = ANCHO_VENTANA
+    alto = ALTO_MARCO_USUARIO + ALTO_FILA * total_filas
+    ancho_pantalla = root.winfo_screenwidth()
+    alto_pantalla = root.winfo_screenheight()
+    x_central = (ancho_pantalla - ancho) // 2
+    y_central = (alto_pantalla - alto) // 2
+    root.geometry(f"{ancho}x{alto}+{x_central}+{y_central}")
     root.mainloop()
