077009782955c55bd48c0067545d99389ce9ea44
mnapo <napolimatiase@gmail.com>
2025-06-23 04:08:31 -0300

    añadir método reset

--- Autenticacion/Sesion.py
+++ Autenticacion/Sesion.py
@@ -5,11 +5,19 @@ class Sesion:
         self.rol_usuario = 0
         self.tipo_rol = None
 
-    def conectar(self, email, rol, tipo_rol):
+    def conectar(self, email, nombre, rol, tipo_rol):
         self.conectado = True
         self.email_usuario = email
+        self.nombre_usuario = nombre
         self.rol_usuario = rol
         self.tipo_rol = tipo_rol
 
-    def desconectar(self):
+    def reset(self):
         self.conectado = False
+        self.email_usuario = ""
+        self.nombre_usuario = ""
+        self.rol_usuario = 0
+        self.tipo_rol = None
+
+    def desconectar(self):
+        self.reset()
