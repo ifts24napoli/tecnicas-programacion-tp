e2d785de655dc791f4dded0df9980f36aa601492
mnapo <napolimatiase@gmail.com>
2025-06-23 03:28:10 -0300

    Corregir consulta SQL

--- Autenticacion/autenticacion.py
+++ Autenticacion/autenticacion.py
@@ -4,7 +4,7 @@ from controlador.Usuarios import consultas
 def verificar_usuario(email, password):
     usuario = Usuarios()
     usuario.mail = email
-    consulta_usuario = consultas("select id_usuario, usuarios.id_rol, tipo_rol from usuarios inner join roles on roles.id_rol = usuarios.id_rol where mail='"+email+"'")
+    consulta_usuario = consultas("select id_usuario, U.id_rol, R.tipo_rol from usuarios as U inner join roles as R on R.id_rol = U.id_rol where mail='"+email+"'")
     if consulta_usuario:
         rol_usuario = consulta_usuario[0][1]
         tipo_rol = consulta_usuario[0][2]
