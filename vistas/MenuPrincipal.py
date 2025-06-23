0cbb718bf5bac8fe0dc6de2897efc1eaa063b682
mnapo <napolimatiase@gmail.com>
2025-06-23 05:11:06 -0300

    Inicializar globales

--- vistas/MenuPrincipal.py
+++ vistas/MenuPrincipal.py
@@ -13,23 +13,25 @@ from vistas.ABMPlanes import abmPlanes
 from vistas.ABMTipoPago import abmTipoPago
 from vistas.ReporteFinanciero import menu as reporteFinanciero 
 
+STICKY_PROP = "w"
 TEXTO_CERRAR_SESION = "Cerrar Sesión"
-TEXTO_SESION_USUARIO = "Bienvenido/a "
-TEXTO_ROLES = "Rol "
 TEXTO_CLIENTES = "Gestión Clientes"
-TEXTO_USUARIOS = "Gestión Usuarios"
-TEXTO_PLANES = "Gestión Planes"
-TEXTO_INVENTARIO = "Gestión Inventarios"
-TEXTO_TIPOS_PAGOS = "Gestión Tipo de Pagos"
-TEXTO_CONTRATO = "Gestión Contratos"
 TEXTO_COMODATOS= "Gestión Comodatos"
+TEXTO_CONTRATO = "Gestión Contratos"
 TEXTO_FACTURAS= "Gestión Facturas"
+TEXTO_INVENTARIO = "Gestión Inventarios"
+TEXTO_PLANES = "Gestión Planes"
 TEXTO_REPORTES= "Gestión Reportes"
+TEXTO_ROLES = "Rol "
+TEXTO_SESION_USUARIO = "Bienvenido/a "
+TEXTO_TIPOS_PAGOS = "Gestión Tipo de Pagos"
 VENTANA_TITULO = "Menú Principal"
+TEXTO_USUARIOS = "Gestión Usuarios"
+STATE_DISABLED = "disabled"
+STATE_ENABLED = "enabled"
 
 def despejar():
     os.system("cls" if os.name == "nt" else "clear")
-    
     
 def creoMenu(sesion:Sesion):
     menu = ['Hola: ' + sesion.email_usuario]
@@ -70,69 +72,73 @@ def GuiMenuPrincipal(sesion:Sesion):
     from Ingreso import main
     root = Tk()
     root.title(VENTANA_TITULO)
-    root.resizable(width=False, height=False)
-    root.geometry("250x400")
+    #marco_usuario = ttk.Frame(root, bg="lightgray", bd=2, relief=ttk.SOLID)
+    #marco_usuario.pack(padx=10, pady=10)
     frm = ttk.Frame(root, padding=10)
     frm.grid()
-    texto_sesion_usuario = ttk.Label(frm, text=TEXTO_SESION_USUARIO+sesion.nombre_usuario)
-    texto_sesion_usuario.grid(column=0, row=1, sticky="w")
-    texto__ROLES = ttk.Label(frm, text=TEXTO_ROLES+sesion.tipo_rol)
-    texto__ROLES.grid(column=0, row=2, sticky="w")
+    marco_usuario = ttk.Frame(frm)
+    marco_usuario.pack(side=TOP)
+    root.resizable(width=False, height=False)
+    root.geometry("250x400")
+    texto_sesion_usuario = ttk.Label(marco_usuario, text=TEXTO_SESION_USUARIO+sesion.nombre_usuario)
+    texto_sesion_usuario.grid(column=0, row=1, sticky=STICKY_PROP)
+    texto__ROLES = ttk.Label(marco_usuario, text=TEXTO_ROLES+sesion.tipo_rol)
+    texto__ROLES.grid(column=0, row=2, sticky=STICKY_PROP)
     btn_usuarios = ttk.Button(frm, text=TEXTO_USUARIOS, command=MenuUsuario)
-    btn_usuarios.grid(column=0, row=3, sticky="w")
+    btn_usuarios.grid(column=0, row=3, sticky=STICKY_PROP)
     btn_clientes = ttk.Button(frm, text=TEXTO_CLIENTES, command=MenuCliente)
-    btn_clientes.grid(column=0, row=4, sticky="w")
+    btn_clientes.grid(column=0, row=4, sticky=STICKY_PROP)
     btn_planes = ttk.Button(frm, text=TEXTO_PLANES, command=lambda: abmPlanes())
-    btn_planes.grid(column=0, row=5, sticky="w")
+    btn_planes.grid(column=0, row=5, sticky=STICKY_PROP)
     btn_inventario = ttk.Button(frm, text=TEXTO_INVENTARIO, command=lambda: abmInventario())
-    btn_inventario.grid(column=0, row=6, sticky="w")
+    btn_inventario.grid(column=0, row=6, sticky=STICKY_PROP)
     btn_tipo_pagos = ttk.Button(frm, text=TEXTO_TIPOS_PAGOS, command=lambda: abmTipoPago())
-    btn_tipo_pagos.grid(column=0, row=7, sticky="w")
+    btn_tipo_pagos.grid(column=0, row=7, sticky=STICKY_PROP)
     btn_contratos = ttk.Button(frm, text=TEXTO_CONTRATO, command=lambda: abmContratos())
-    btn_contratos.grid(column=0, row=8, sticky="w")
+    btn_contratos.grid(column=0, row=8, sticky=STICKY_PROP)
     btn_comodatos = ttk.Button(frm, text=TEXTO_COMODATOS, command=lambda: abmComodato())
-    btn_comodatos.grid(column=0, row=9, sticky="w")
+    btn_comodatos.grid(column=0, row=9, sticky=STICKY_PROP)
     btn_facturas = ttk.Button(frm, text=TEXTO_FACTURAS, command=lambda: abmFacturacion())
-    btn_facturas.grid(column=0, row=10, sticky="w")
+    btn_facturas.grid(column=0, row=10, sticky=STICKY_PROP)
     btn_reportes = ttk.Button(frm, text=TEXTO_REPORTES, command=lambda: reporteFinanciero())
-    btn_reportes.grid(column=0, row=11, sticky="w")
+    btn_reportes.grid(column=0, row=11, sticky=STICKY_PROP)
     
-    btn_usuarios.config(state="disabled")
-    btn_clientes.config(state="disabled")
-    btn_planes.config(state="disabled")
-    btn_inventario.config(state="disabled")
-    btn_tipo_pagos.config(state="disabled")
-    btn_contratos.config(state="disabled")
-    btn_comodatos.config(state="disabled")
-    btn_facturas.config(state="disabled")
-    btn_reportes.config(state="disabled")
+    btn_usuarios.config(state=STATE_DISABLED)
+    btn_clientes.config(state=STATE_DISABLED)
+    btn_planes.config(state=STATE_DISABLED)
+    btn_inventario.config(state=STATE_DISABLED)
+    btn_tipo_pagos.config(state=STATE_DISABLED)
+    btn_contratos.config(state=STATE_DISABLED)
+    btn_comodatos.config(state=STATE_DISABLED)
+    btn_facturas.config(state=STATE_DISABLED)
+    btn_reportes.config(state=STATE_DISABLED)
     
     match sesion.rol_usuario:
         case 1: 
-                btn_usuarios.config(state="enabled")
-                btn_clientes.config(state="enabled")
-                btn_planes.config(state="enabled")
-                btn_inventario.config(state="enabled")
-                btn_tipo_pagos.config(state="enabled")
-                btn_contratos.config(state="enabled")
-                btn_comodatos.config(state="enabled")
-                btn_facturas.config(state="enabled")
-                btn_reportes.config(state="enabled")
+                btn_usuarios.config(state=STATE_ENABLED)
+                btn_clientes.config(state=STATE_ENABLED)
+                btn_planes.config(state=STATE_ENABLED)
+                btn_inventario.config(state=STATE_ENABLED)
+                btn_tipo_pagos.config(state=STATE_ENABLED)
+                btn_contratos.config(state=STATE_ENABLED)
+                btn_comodatos.config(state=STATE_ENABLED)
+                btn_facturas.config(state=STATE_ENABLED)
+                btn_reportes.config(state=STATE_ENABLED)
         case 2:
-                btn_clientes.config(state="enabled")
-                btn_planes.config(state="enabled")
-                btn_inventario.config(state="enabled")
-                btn_tipo_pagos.config(state="enabled")
-                btn_contratos.config(state="enabled")
-                btn_comodatos.config(state="enabled") 
+                btn_clientes.config(state=STATE_ENABLED)
+                btn_planes.config(state=STATE_ENABLED)
+                btn_inventario.config(state=STATE_ENABLED)
+                btn_tipo_pagos.config(state=STATE_ENABLED)
+                btn_contratos.config(state=STATE_ENABLED)
+                btn_comodatos.config(state=STATE_ENABLED) 
         case 3:
-                btn_facturas.config(state="enabled")
-                btn_reportes.config(state="enabled")             
+                btn_facturas.config(state=STATE_ENABLED)
+                btn_reportes.config(state=STATE_ENABLED)             
     
     def cerrar_sesion():
         sesion.desconectar()
         root.destroy()
         main()
-    btn_cerrar_sesion = ttk.Button(frm, text=TEXTO_CERRAR_SESION, command=cerrar_sesion)
-    btn_cerrar_sesion.grid(column=0, row=12, sticky="w")
+    btn_cerrar_sesion = ttk.Button(marco_usuario, text=TEXTO_CERRAR_SESION, command=cerrar_sesion)
+    btn_cerrar_sesion.grid(column=0, row=12, sticky=STICKY_PROP)
     root.mainloop()
